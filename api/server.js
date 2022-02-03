29987fcc2058312a032decdc11b111e6da23d14b
Phil G <fillupgalbraith@gmail.com>
2022-02-02 20:28:03 -0800

    heroku push

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-
+server.options('*', cors())
 server.use('/api/users', usersRouter)
 server.use('/api/recipes', recipesRouter)
 
