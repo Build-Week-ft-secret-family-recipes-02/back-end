95b6146948824768ae43fdc275fef3f9b0f2007e
Phil G <fillupgalbraith@gmail.com>
2022-02-03 12:08:14 -0800

    heroku push

--- api/users/router.js
+++ api/users/router.js
@@ -14,8 +14,12 @@ router.get('/', async (req, res) => {
 // http --pretty='none' :9000/api/users
 
 router.post('/', async (req, res) => {
-  const user = await insertUser(req.body)
-  res.status(201).json(user)
+   try{
+    const user = await insertUser(req.body)
+    res.status(201).json(user)
+   } catch (error) {
+    res.status(500).json(error)
+   }
 })
 // http --pretty='none' :9000/api/users username='username10' password='password10'
 router.post('/login', async (req, res) => {
